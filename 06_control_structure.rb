1755b06a5f7cbd0465b2b706f43058a0bc99fac3
Huang Yi-Ming <ymhuang@citi.sinica.edu.tw>
2015-01-26 14:54:37 +0800

    加入if-else 控制

---
+++ 06_control_structure.rb
@@ -0,0 +1,16 @@
+my_num1 = 50
+my_num2 = -20
+
+if my_num1 > 0
+  puts "#{my_num1} is positive"
+else
+  puts "#{my_num1} is negative"
+end
+
+if my_num2 > 0
+  puts "#{my_num2} is positive"
+else
+  puts "#{my_num2} is negative"
+end
+
+# recommendation: Use method
