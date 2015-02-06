d0a9fc282d72df4aca705514a23114a4dc359418
Huang Yi-Ming <ymhuang@citi.sinica.edu.tw>
2015-02-06 18:02:59 +0800

    Add 09 and 10

---
+++ 09_refactor_eg07.rb
@@ -0,0 +1,13 @@
+def posi_nega_number(my_num)
+  if my_num > 0
+    puts "#{my_num} is positive"
+  else
+    puts "#{my_num} is negative"
+  end
+end
+
+my_num1 = 50
+my_num2 = -20
+
+posi_nega_number(my_num1)
+posi_nega_number(my_num2)
