d17d56a8633f5df1f65d50a1c8f2c4e7e95f5f60
catx4 <cat4@citi.sinica.edu.tw>
2015-02-12 15:50:34 +0800

    new homework

--- homework/week1.rb
+++ homework/week1.rb
@@ -5,21 +5,26 @@ puts "你好，你叫什麼名字呢？"
 name =  gets.chomp
 puts "#{name}你好！我是殘酷的計算小精靈，要來幫你算 BMI 囉！"
 puts "請輸入你的身高（單位：公尺）"
-height= gets.chomp
+height= gets.chomp.to_f
 puts "請輸入你的體重（單位：公斤）" 
-weight = gets.chomp
+weight = gets.chomp.to_f
 sum=height*height
-BMI="#{weight/sum}"
+BMI=weight/sum
 puts "你的 BMI值是#{BMI}"
+
+if name=="阿銘"
+	puts "Jim說看到阿銘就要顯示過重"
+end
+
+
 if  BMI<18.5
                 puts "紙片人！"
-elsif  18.5<=BMI<24
+elsif  18.5<=BMI && BMI<24
                 puts "正常！保持下去吧！"
-elsif  24<=BMT<27
+elsif  24<=BMI && BMI <27
 	puts "過重，要多運動囉！"
 elsif BMI>27
 	puts "連勝文？"
-elsif  name=阿銘
-	puts "Jim說看到阿銘就要顯示過重"
-	
+
 end
+
